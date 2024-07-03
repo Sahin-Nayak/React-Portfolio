4e643d254ce3a3c61931ae12c615dd49df2d4a1e
Sahin Nayak <89744970+Sahin-Nayak@users.noreply.github.com>
2024-07-03 14:03:02 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am Sahin Nayak. I live in Kolkata.I am A student of University Of Engineering and Management. Now I am Pursuing my MCA. And I completed my higher secondary and secondary examination from Laban Hrad Vidyapith.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web designer",
+  "Java Developer",
+  "Voice Artist",
+  "Sound Designer",
+  "Software Developer",
 ];
 
 /**
